debfa8eebb8f704bd6b31bccc187b7d7849858c7
Uddhriti <74648318+Uddhriti@users.noreply.github.com>
2021-03-05 13:52:35 +0530

    Add files via upload

--- main2.js
+++ main2.js
@@ -56,23 +56,23 @@ function check()
 	if(question_turn == "player1")
 	{
 		question_turn = "player2"
-		document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name ;
+		document.getElementById("question_turn").innerHTML = "Question Turn - " + player2_name ;
 	}
 	else 
 	{
 		question_turn = "player1"
-		document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+		document.getElementById("question_turn").innerHTML = "Question Turn - " + player1_name ;
 	}
 
 	if(answer_turn == "player1")
 	{
 		answer_turn = "player2"
-		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+		document.getElementById("answer_turn").innerHTML = "Answer Turn - " + player2_name ;
 	}
 	else 
 	{
 		answer_turn = "player1"
-		document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name ;
+		document.getElementById("answer_turn").innerHTML = "Answer Turn - " + player1_name ;
 	}
 
     
